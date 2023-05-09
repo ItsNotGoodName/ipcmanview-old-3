39a4cab6f52d39a3f9e201fd087437ff30629d6b
ItsNotGoodName <gurnaindeol@gmail.com>
2023-05-09 11:55:42 -0700

    fix(ui): remove echo dep

--- ipcmanview-ui/embed.go
+++ ipcmanview-ui/embed.go
@@ -4,11 +4,22 @@ package ui
 
 import (
 	"embed"
-
-	"github.com/labstack/echo/v5"
+	"fmt"
+	"io/fs"
+	"path/filepath"
 )
 
 //go:embed dist
 var dist embed.FS
 
-var FS = echo.MustSubFS(dist, "dist")
+var FS = mustSubFS(dist, "dist")
+
+// https://github.com/labstack/echo/blob/deb17d2388a74cd4133f46c2dedfb7601da1db0a/echo_fs.go#LL144C2-L144C2
+func mustSubFS(currentFs fs.FS, fsRoot string) fs.FS {
+	fsRoot = filepath.ToSlash(filepath.Clean(fsRoot)) // note: fs.FS operates only with slashes. `ToSlash` is necessary for Windows
+	subFs, err := fs.Sub(currentFs, fsRoot)
+	if err != nil {
+		panic(fmt.Errorf("can not create sub FS, invalid root given, err: %w", err))
+	}
+	return subFs
+}
--- ipcmanview-ui/embed_dev.go
+++ ipcmanview-ui/embed_dev.go
@@ -6,10 +6,10 @@ import (
 	"io/fs"
 )
 
+var FS = empty{}
+
 type empty struct{}
 
 func (empty) Open(string) (fs.File, error) {
 	return nil, fs.ErrNotExist
 }
-
-var FS = empty{}
